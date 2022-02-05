42220b73903e132e77a831680d2882a8237021e3
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2022-02-05 15:28:44 +0100

    open ai

---
+++ OpenAI/OpenAI/Program.cs
@@ -0,0 +1,17 @@
+﻿using OpenAI;
+
+var ai = new OpenAIClient(Engine.Davinci);
+
+while (true)
+{
+    Console.Write("Input: ");
+    var input = Console.ReadLine();
+
+    if (input == null) { break; }
+
+    var result = await ai.CompletionEndpoint.CreateCompletionAsync(input, max_tokens: 64);
+
+    var textResult = result.Completions.First().Text;
+
+    Console.WriteLine($"The result is {textResult}");
+}
